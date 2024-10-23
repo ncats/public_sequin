4d0d7401b2625a4f336960c3d899cdb70423e935
chevvak2 <114002880+chevvak2@users.noreply.github.com>
2024-10-23 10:08:30 -0400

    Update sql.R

--- sql.R
+++ sql.R
@@ -1,27 +1,13 @@
 # sc
-#usr_sc <- Sys.getenv('usr_sc')
-#usr_sup <- Sys.getenv('usr_sup')
-#scdb <- Sys.getenv('scdb')
+usr_sc <- Sys.getenv('usr_sc')
+usr_sup <- Sys.getenv('usr_sup')
+scdb <- Sys.getenv('scdb')
 
 # bulk
-#usr_bulk <- Sys.getenv('usr_bulk')
-#bdb <- Sys.getenv('bdb')
-#pwd_sc <- Sys.getenv('pwd_sc')
-#pwd_bulk <- Sys.getenv('pwd_bulk')
-#pwd_sc_sup <- Sys.getenv('pwd_sc_sup')
-#ec_host <- Sys.getenv('ec_host')
-#p <- 3306
-
-# sc
-Sys.getenv(usr_sc = "usr_sc")
-Sys.getenv(usr_sc = "usr_sup")
-Sys.getenv(scdb = "scdb")
-
-# bulk
-Sys.getenv(usr_bulk = "usr_bulk")
-Sys.getenv(bdb = "bdb")
-Sys.getenv(pwd_sc = "pwd_sc")
-Sys.getenv(pwd_bulk = "pwd_bulk")
-Sys.getenv(pwd_sc_sup = "pwd_sc_sup")
-Sys.getenv(ec_host = "ec_host")
+usr_bulk <- Sys.getenv('usr_bulk')
+bdb <- Sys.getenv('bdb')
+pwd_sc <- Sys.getenv('pwd_sc')
+pwd_bulk <- Sys.getenv('pwd_bulk')
+pwd_sc_sup <- Sys.getenv('pwd_sc_sup')
+ec_host <- Sys.getenv('ec_host')
 p <- 3306
